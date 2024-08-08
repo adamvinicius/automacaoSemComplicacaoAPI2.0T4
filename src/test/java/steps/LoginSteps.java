03c8c2041b31582503b31fad743cae5ba274fcc2
Adam Vinicius <adamvinicius@gmail.com>
2024-08-08 20:41:50 -0300

    aula 20, jenkins

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -10,18 +10,16 @@ import utils.RestUtils;
 import java.util.Map;
 
 public class LoginSteps {
-    String url = "http://localhost:8080/";
+
 
     @Dado("que tenha um payload valido da API de Login")
     public void queTenhaUmPayloadValidoDaAPIDeLogin() {
         LoginMap.initLogin();
-        RestUtils.setBaseURI(url);
     }
 
     @Dado("que tenha um payload da API de Login com as seguintes informacoes")
     public void queTenhaUmPayloadDaAPIDeLoginComAsSeguintesInformacoes(Map<String, String> map) {
         LoginMap.initLogin();
-        RestUtils.setBaseURI(url);
         LoginMap.getLogin().putAll(map);
     }
 
